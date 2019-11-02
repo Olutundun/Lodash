e84ed53929abb49f5436085faee78ed76de1066e
Your Name <tundun.deborah@gmail.com>
2019-11-02 19:25:21 -0400

    "added clamp method"

--- _.js
+++ _.js
@@ -1,8 +1,11 @@
-const _ = {};
-clamp()
-
-
-
-
+const _ = {
+    //clamp modifies the provided number to be within the two provided bounds
+    clamp(number, lower, upper) {
+      const lowerClampedValue = Math.max(number, lower);
+      const clampedValue = Math.min(lowerClampedValue, upper);
+       return clampedValue;
+    }
+  };
+  
 
 module.exports = _;
