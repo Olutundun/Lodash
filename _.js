51833c8ede23d70f5c1657382cc8071d6d90d075
Your Name <tundun.deborah@gmail.com>
2019-11-02 20:16:12 -0400

    "added inRange metthod"

--- _.js
+++ _.js
@@ -4,8 +4,22 @@ const _ = {
       const lowerClampedValue = Math.max(number, lower);
       const clampedValue = Math.min(lowerClampedValue, upper);
        return clampedValue;
+    },
+    //inRange checks if n is between start and up to, but not including, end. 
+    //If end is not specified, it's set to start with start then set to 0. 
+    //If start is greater than end the params are swapped to support negative ranges.
+    inRange(number, start, end) {
+      if (end === undefined) {
+        end = start;
+        start = 0;
+      } else if (start > end) {
+        const temp = end;
+        end = start;
+        start = temp;
+      }
+      const isInRange = start <= number && number < end;
+      return isInRange;
     }
   };
-  
 
 module.exports = _;
